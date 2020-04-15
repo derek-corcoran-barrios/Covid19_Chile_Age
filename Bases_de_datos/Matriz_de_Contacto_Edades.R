558a882555c4d4d0b062863f86bd961ea0ab1e44
derek-corcoran-barrios <derek.corcoran.barrios@gmailcom>
2020-04-15 16:26:18 -0400

    Primer Commit

---
+++ Bases_de_datos/Matriz_de_Contacto_Edades.R
@@ -0,0 +1,36 @@
+setwd("~/Documents/Covid19_Chile_Age")
+
+## Cargaremos los paquetes necesarios?contact_matrix
+
+library(tidyverse)
+library(readxl)
+
+
+##Leemos los estimadores de Chile
+
+Estimadores <- read_excel("Bases_de_datos/contact_matrices_152_countries/MUestimates_all_locations_1.xlsx", 
+                                          sheet = "Chile")
+
+
+##Agregamos como nombres de columa las edades correspondientes
+
+colnames(Estimadores) <- paste0("Age_",paste(c(0:15)*5, c(1:16)*5, sep = "_")) 
+rownames(Estimadores) <- paste0("Age_",paste(c(0:15)*5, c(1:16)*5, sep = "_")) 
+
+## Leemos la base de datos de cantidad de personas por sexo y edad
+Cantidad_Edad <- read_excel("Bases_de_datos/Cantidad-de-Personas-por-Sexo-y-Edad.xlsx",sheet = "COMUNAS")
+colnames(Cantidad_Edad) <- make.names(colnames(Cantidad_Edad))
+
+## Nos quedamos con solo lo de todo el país
+Pais <- Cantidad_Edad %>%  dplyr::filter(NOMBRE.REGIÓN == "PAÍS", Edad != "Total País") %>% 
+  ## Nos quedamos solo con los grupos de edad y los Totales poblacionales
+  dplyr::select(Edad, TOTAL) %>% 
+  ## Reagrupamos segun los grupos etareos que nos interesan
+  dplyr::mutate(Grupo = case_when(Edad %in% c("0 a 4", "5 a 9", "10 a 14", "15 a 19", "20 a 24") ~ "0_24",
+                                  Edad %in% c("25 a 29","30 a 34", "35 a 39", "40 a 44", "45 a 49", "50 a 54", "55 a 59", "60 a 64") ~ "25_65",
+                                  Edad %in% c("65 a 69", "70 a 74", "75 a 79", "80 a 84", "85 a 89","90 a 94", "95 a 99", "100 o más") ~ "65+")) %>% 
+  # sumamos la poblacion  y calculamos la proporcion de cada grupo
+  mutate(Poblacion = sum(TOTAL), Prop = TOTAL/Poblacion)
+  
+Data_Final <- data.frame(Age_0_25 = rep(NA, 3), Age_25_65 = rep(NA, 3), Age_65_ = rep(NA, 3))
+
---
+++ README.md
@@ -0,0 +1,49 @@
+Organización de modelos para covid19 usando
+================
+
+## Paquetes necesarios para el uso completo de este repositorio
+
+  - **tidyverse**
+  - **readxl**
+
+## Generación de bases de datos y estimación de parametros
+
+### Cantidad de contactos diarios y matriz de contactos entre edades
+
+Para determinar la cantidad de contactos diarios y la matriz de
+contactos entre edades utilizaremos el sript
+`Matriz_de_Contacto_Edades.R`, utilizamos la base de datos de Prem,
+Cook, and Jit (2017) como base recalculando para los grupos de edad que
+usamos (0-25, 25-65 y 65+).
+
+El primer resultado, que encontramos en las lineas 31-37 del código y
+guardadas en `Bases_de_datos/Contactos.rds` nos dice que para los grupos
+antes establecidos en número de contactos diarios promedio es de 18.44,
+14.3, 1.91 por persona respectivamente
+
+### Probabilidad de contactos entre comunas
+
+Para esto
+
+### Estado inicial del modelo
+
+## Modelaciones
+
+## Resultados
+
+## Difusión
+
+## Referencias
+
+<div id="refs" class="references">
+
+<div id="ref-prem2017projecting">
+
+Prem, Kiesha, Alex R Cook, and Mark Jit. 2017. “Projecting Social
+Contact Matrices in 152 Countries Using Contact Surveys and Demographic
+Data.” *PLoS Computational Biology* 13 (9). Public Library of Science:
+e1005697.
+
+</div>
+
+</div>
