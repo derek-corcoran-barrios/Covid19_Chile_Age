9f69ce2c1d6453b6d3aa4355981f92d2762a435f
derek-corcoran-barrios <derek.corcoran.barrios@gmailcom>
2020-04-15 17:04:41 -0400

    Agregamos dos bases de datos

--- Bases_de_datos/Matriz_de_Contacto_Edades.R
+++ Bases_de_datos/Matriz_de_Contacto_Edades.R
@@ -21,6 +21,21 @@ rownames(Estimadores) <- paste0("Age_",paste(c(0:15)*5, c(1:16)*5, sep = "_"))
 Cantidad_Edad <- read_excel("Bases_de_datos/Cantidad-de-Personas-por-Sexo-y-Edad.xlsx",sheet = "COMUNAS")
 colnames(Cantidad_Edad) <- make.names(colnames(Cantidad_Edad))
 
+## Ponemos los nombres de grupo de edad entre cada grupo etareo 
+Grupo1 <- c("Age_0_5", "Age_5_10", "Age_10_15", "Age_15_20", "Age_20_25")
+Grupo2 <- c("Age_25_30", "Age_30_35", "Age_35_40", "Age_40_45", "Age_45_50","Age_50_55", "Age_55_60", "Age_60_65")
+Grupo3 <- c("Age_65_70", "Age_70_75","Age_75_80")
+
+## Estimación de numero de contactos por grupo etareo
+
+Contactos_Grupo_1 <- weighted.mean(colSums(Estimadores[Grupo1]), w = Pais$TOTAL[1:5])
+Contactos_Grupo_2 <- weighted.mean(colSums(Estimadores[Grupo2]), w = Pais$TOTAL[6:13])
+Contactos_Grupo_3 <- weighted.mean(colSums(Estimadores[Grupo3]), w = Pais$TOTAL[14:16])
+
+Contactos <- c(Contactos_Grupo_1, Contactos_Grupo_2, Contactos_Grupo_3)
+
+saveRDS(Contactos, "Bases_de_datos/Contactos.rds")
+
 ## Nos quedamos con solo lo de todo el país
 Pais <- Cantidad_Edad %>%  dplyr::filter(NOMBRE.REGIÓN == "PAÍS", Edad != "Total País") %>% 
   ## Nos quedamos solo con los grupos de edad y los Totales poblacionales
@@ -33,4 +48,21 @@ Pais <- Cantidad_Edad %>%  dplyr::filter(NOMBRE.REGIÓN == "PAÍS", Edad != "Tot
   mutate(Poblacion = sum(TOTAL), Prop = TOTAL/Poblacion)
   
 Data_Final <- data.frame(Age_0_25 = rep(NA, 3), Age_25_65 = rep(NA, 3), Age_65_ = rep(NA, 3))
+
+Data_Final$Age_0_25[1] <- weighted.mean(colSums(Estimadores[1:5,Grupo1]), Pais$TOTAL[1:5])/Contactos[1] 
+Data_Final$Age_25_65[1] <- weighted.mean(colSums(Estimadores[6:13,Grupo1]), Pais$TOTAL[1:5])/Contactos[1]
+Data_Final$Age_65_[1] <- weighted.mean(colSums(Estimadores[14:16,Grupo1]), Pais$TOTAL[1:5])/Contactos[1]
+
+Data_Final$Age_0_25[2] <- weighted.mean(colSums(Estimadores[1:5,Grupo2]), Pais$TOTAL[6:13])/Contactos[2] 
+Data_Final$Age_25_65[2] <- weighted.mean(colSums(Estimadores[6:13,Grupo2]), Pais$TOTAL[6:13])/Contactos[2]
+Data_Final$Age_65_[2] <- weighted.mean(colSums(Estimadores[14:16,Grupo2]), Pais$TOTAL[6:13])/Contactos[2]
+
+Data_Final$Age_0_25[3] <- weighted.mean(colSums(Estimadores[1:5,Grupo3]), Pais$TOTAL[14:16])/Contactos[3] 
+Data_Final$Age_25_65[3] <- weighted.mean(colSums(Estimadores[6:13,Grupo3]), Pais$TOTAL[14:16])/Contactos[3]
+Data_Final$Age_65_[3] <- weighted.mean(colSums(Estimadores[14:16,Grupo3]), Pais$TOTAL[14:16])/Contactos[3]
+
+Data_Final <- as.matrix(Data_Final)
+rownames(Data_Final) <- colnames(Data_Final)
+
+saveRDS(Data_Final, "Bases_de_datos/Age_Matrix.rds")
 
--- README.md
+++ README.md
@@ -19,11 +19,27 @@ usamos (0-25, 25-65 y 65+).
 El primer resultado, que encontramos en las lineas 31-37 del código y
 guardadas en `Bases_de_datos/Contactos.rds` nos dice que para los grupos
 antes establecidos en número de contactos diarios promedio es de 18.44,
-14.3, 1.91 por persona respectivamente
+14.3, 1.91 por persona respectivamente.
+
+La matriz de probabilidad de contacto entre grupos etareos aparece en la
+linea 64, y esta guardada como `Bases_de_datos/Age_Matrix.rds`. donde en
+cada fila, se ve la probabilidad de que el grupo en cuestion este en
+contacto con el de cada
+columna:
+
+``` r
+knitr::kable(read_rds("Bases_de_datos/Age_Matrix.rds"), digits = 2, booktabs = T)
+```
+
+|             | Age\_0\_25 | Age\_25\_65 | Age\_65\_ |
+| ----------- | ---------: | ----------: | --------: |
+| Age\_0\_25  |       0.57 |        0.37 |      0.06 |
+| Age\_25\_65 |       0.25 |        0.67 |      0.08 |
+| Age\_65\_   |       0.18 |        0.37 |      0.45 |
 
 ### Probabilidad de contactos entre comunas
 
-Para esto
+Para esto hay dos scripts generados, uno que pe
 
 ### Estado inicial del modelo
 
